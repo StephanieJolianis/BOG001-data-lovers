b3cb639ecfff6642a830c0f977d8215f049a4fe1
Caro <cslondo90@gmail.com>
2020-06-26 11:56:58 -0500

    impresión de imágenes

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@ import data from './data/pokemon/pokemon.js';
 
 
 console.log(example, data);
+
+
+//document.getElementById("root").src = data.pokemon[0]["img"];
+
+var x = document.createElement("IMG");
+x.setAttribute("src", data.pokemon[0]["img"]);
+x.setAttribute("width", "200");
+x.setAttribute("height", "200");
+document.body.appendChild(x);
