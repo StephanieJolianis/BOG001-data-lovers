cae346076ce0c3c34596c41ad9cdab86c278b16a
Caro <cslondo90@gmail.com>
2020-06-29 20:38:54 -0500

    historia de usuario 1 29/06/20

--- src/main.js
+++ src/main.js
@@ -1,15 +1,36 @@
 import { data2 } from './data.js';
 
 let result = data2();
-//console.log(result);
-//document.getElementById("root").src = data.pokemon[0]["img"];
-document.getElementById("root");
-  for (let prueba2 of result){
-    var x = document.createElement("IMG");
-    x.setAttribute("src", prueba2.img);
-    x.setAttribute("width", "200");
-    x.setAttribute("height", "200");
-    document.body.appendChild(x);
-    
-  }
 
+var rootNode=document.getElementById("root");
+
+for (let pokemons of result){
+
+  var divPokemon=document.createElement("div");
+  var divPokemonText=document.createElement("div");
+  var imagePokemon=document.createElement("IMG");
+  var pokemonNumberH1= document.createElement("h1");
+  var pokemonNameP= document.createElement("p");
+  var pokemonTypeP= document.createElement("p");
+
+  var pokemonNumber=document.createTextNode(pokemons.num);
+  var pokemonName=document.createTextNode(pokemons.name);
+  var pokemonType=document.createTextNode(pokemons.type);
+  
+  imagePokemon.setAttribute("src", pokemons.img);
+  divPokemon.setAttribute("class", "container");
+  pokemonNameP.setAttribute("class", "pokemonNameStyle");
+  
+  pokemonNumberH1.appendChild(pokemonNumber);
+  pokemonNameP.appendChild(pokemonName);
+  pokemonTypeP.appendChild(pokemonType);
+
+  divPokemonText.appendChild(pokemonNumberH1);
+  divPokemonText.appendChild(pokemonNameP);
+  divPokemonText.appendChild(pokemonTypeP);
+  
+  divPokemon.appendChild(imagePokemon);
+  divPokemon.appendChild(divPokemonText);
+  
+  rootNode.appendChild(divPokemon);
+}
