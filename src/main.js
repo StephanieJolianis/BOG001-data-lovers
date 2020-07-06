bd7914aae7b464deb34ed23a7982fa5892ffde5d
Caro <cslondo90@gmail.com>
2020-07-06 16:04:44 -0500

    Nuevo Inicio de Proyecto

--- src/main.js
+++ src/main.js
@@ -1,17 +1,6 @@
 import { data2 } from './data.js';
 
 let result = data2();
-
-/*let pokemoncard2 = ("");
-  for (let prueba2 of result){
-    let pokemoncard1 = '<div class="' + prueba2.type[0] + '"><span class="name">' + prueba2.name +  '</span><span class="num">' + prueba2.num + '</span><div class="img"><img src=' +  prueba2.img + '></div><div class=type><span class="type1">' + prueba2.type+'</span></div></div>';
-  pokemoncard2= pokemoncard2 + pokemoncard1;
-    
-  }
-  let root = document.getElementById("root").innerHTML;
-  document.getElementById("root").innerHTML = pokemoncard2;*/
-
-
 let rootNode = document.getElementById("root");
 
 for (let pokemon of result) {
@@ -30,7 +19,6 @@ for (let pokemon of result) {
   let img = document.createElement("div")
   img.setAttribute("class", "img");
 
-
   let img1 = document.createElement("img");
   img1.setAttribute("src", pokemon.img);
 
@@ -47,6 +35,7 @@ for (let pokemon of result) {
   type1.appendChild(types);
   img.appendChild(img1);
   type.appendChild(type1);
+  
   pokemon1.appendChild(name);
   pokemon1.appendChild(num);
   pokemon1.appendChild(img);
@@ -55,37 +44,3 @@ for (let pokemon of result) {
   rootNode.appendChild(pokemon1);
 }
 
-
-
-var rootNode=document.getElementById("root");
-
-for (let pokemons of result){
-
-  var divPokemon=document.createElement("div");
-  var divPokemonText=document.createElement("div");
-  var imagePokemon=document.createElement("IMG");
-  var pokemonNumberH1= document.createElement("h1");
-  var pokemonNameP= document.createElement("p");
-  var pokemonTypeP= document.createElement("p");
-
-  var pokemonNumber=document.createTextNode(pokemons.num);
-  var pokemonName=document.createTextNode(pokemons.name);
-  var pokemonType=document.createTextNode(pokemons.type);
-  
-  imagePokemon.setAttribute("src", pokemons.img);
-  divPokemon.setAttribute("class", "container");
-  pokemonNameP.setAttribute("class", "pokemonNameStyle");
-  
-  pokemonNumberH1.appendChild(pokemonNumber);
-  pokemonNameP.appendChild(pokemonName);
-  pokemonTypeP.appendChild(pokemonType);
-
-  divPokemonText.appendChild(pokemonNumberH1);
-  divPokemonText.appendChild(pokemonNameP);
-  divPokemonText.appendChild(pokemonTypeP);
-  
-  divPokemon.appendChild(imagePokemon);
-  divPokemon.appendChild(divPokemonText);
-  
-  rootNode.appendChild(divPokemon);
-}
