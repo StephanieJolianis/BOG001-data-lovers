0014174e5ff14f65263cee675352258473c375d0
Caro <cslondo90@gmail.com>
2020-07-16 12:07:21 -0500

    finalizada primera historia por fiiiiiiin!!!!

--- src/main.js
+++ src/main.js
@@ -33,36 +33,28 @@ function openSort(){
 var screenWidth = window.matchMedia("(min-width: 800px)");
 
 var pikachuOnFilter = () => {
-
   document.getElementById("pikachu").style.display="block";
   document.getElementById("pikachu").style.gridColumnStart=2;
   document.getElementById("pikachu").style.gridColumnEnd=3;
-  document.getElementById("filterButton").style.color="#F83500";
-  console.log("sí lee la función pikachuOnFilter")
 }
 
 var pikachuOnSort = () => {
   document.getElementById("pikachu").style.display="block";
   document.getElementById("pikachu").style.gridColumnStart=3;
   document.getElementById("pikachu").style.gridColumnEnd=4;
-  document.getElementById("sortButton").style.color="#F83500";
-  console.log("sí lee la función pikachuOnSort")
 }
 
 var pikachuOnStats = () => {
   document.getElementById("pikachu").style.display="block";
   document.getElementById("pikachu").style.gridColumnStart=4;
   document.getElementById("pikachu").style.gridColumnEnd=5;
-  document.getElementById("pokestatsButton").style.color="#F83500";
-  console.log("sí lee la función pikachuOnSort")
-
 }
-
 
 if (screenWidth.matches) {
   document.getElementById("sortButton").addEventListener("mouseover", pikachuOnSort);
   document.getElementById("filterButton").addEventListener("mouseover", pikachuOnFilter);
   document.getElementById("pokestatsButton").addEventListener("mouseover", pikachuOnStats);
+  
 } else {
   document.getElementById("pikachu").style.gridColumnStart=2;
   document.getElementById("pikachu").style.gridColumnEnd=3;
